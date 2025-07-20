1a5293effa5a1a8df76e06b3fbbb7626598b1727
Kumsal Obuz <iam@denizkumsal.com>
2025-07-20 14:00:16 -0400

    configures dev settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,28 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt(
-  // Your custom configs here
-)
+  // General
+  {
+    plugins: {
+      vue: require('eslint-plugin-vue'),
+    },
+    typescript: true, vue: true,
+    stylistic: {
+      indent: "tab",
+      quotes: "double"
+    },
+    rules: {
+      "vue/block-order": ["error", {
+        order: ["script", "template", "style"]
+      }]
+    }
+  },
+
+  // Vue
+  {
+    files: ["**/*.vue"],
+    rules: {
+      "style/indent": "off"
+    }
+  }
+);
