702183df07ce04b0ab858a113269e97922ead7f1
Kumsal Obuz <iam@denizkumsal.com>
2025-07-27 11:51:07 -0400

    Introduces a tag cloud component

---
+++ components/TagCloud.vue
@@ -0,0 +1,15 @@
+<script setup lang='ts'>
+const props = defineProps<{
+  tags: string[]
+}>()
+const tagsSorted = props.tags.slice().sort((a, b) => {
+  const aIsLower = a[0] === a[0].toLowerCase();
+  const bIsLower = b[0] === b[0].toLowerCase();
+  if (aIsLower && !bIsLower) return -1;
+  if (!aIsLower && bIsLower) return 1;
+  return a.localeCompare(b);
+});
+</script>
+<template>
+  <Tag v-for="tag in tagsSorted" :key="tag" :value="tag" severity="info" />
+</template>
